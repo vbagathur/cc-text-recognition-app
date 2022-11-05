8bb5c920194e998b35d6793e50923de4a0e56960
Contractor Viswanath Natarajan <vnatarajan@llbean.com>
2022-11-05 19:01:25 -0400

    bigtable test code working

--- test-google-bigtable.js
+++ test-google-bigtable.js
@@ -6,7 +6,7 @@ const {Bigtable} = require('@google-cloud/bigtable');
 
 const bigtable = new Bigtable();
 
-async function quickstart() {
+async function writeReadSimple() {
   // Connect to an existing instance:my-bigtable-instance
   const instance = bigtable.instance(process.env.BIGTABLE_INSTANCE_ID);
 
@@ -14,38 +14,36 @@ async function quickstart() {
   const table = instance.table(process.env.BIGTABLE_TABLE_ID);
 
   const timestamp = new Date();
-  const row = table.row('phone#4c410523#20190501');
-  const filter = [
-    {
-      column: 'os_build',
-      value: {
-        start: 'PQ2A',
-        end: 'PQ2A',
-      },
-    },
-  ];
-
-  const config = {
-    onMatch: [
-      {
-        method: 'insert',
-        data: {
-          stats_summary: {
-            os_name: 'android',
-            timestamp,
-          },
+  const rowToInsert = {
+    key: 'phone#4c410523#20190501',
+    data: {
+      stats_summary: {
+        connected_cell: {
+          value: 1,
+          timestamp,
+        },
+        connected_wifi: {
+          value: 1,
+          timestamp,
+        },
+        os_build: {
+          value: 'PQ2A.190405.003',
+          timestamp,
         },
       },
-    ],
+    },
   };
 
-  await row.filter(filter, config);
+  await table.insert(rowToInsert);
+
+  console.log(`Successfully wrote row ${rowToInsert.key}`);
 
   // Read a row from my-table using a row key
-  const [singleRow] = await table.row('r1').get();
+  const [singleRow] = await table.row('phone#4c410523#20190501').get();
 
   // Print the row key and data (column value, labels, timestamp)
   const rowData = JSON.stringify(singleRow.data, null, 4);
   console.log(`Row key: ${singleRow.id}\nData: ${rowData}`);
+
 }
-quickstart();
+writeReadSimple();
